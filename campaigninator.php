2b06d21c372858ee7080f3b530ac9659a481a2b5
Lehman Black <lehman@developforfun.com>
2016-06-08 21:39:11 -0500

    add plugin comment

---
+++ campaigninator.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Campaigninator
+Plugin URI:  http://www.developforfun.com
+Description: Create Google Analytics campaign URLs on pages and posts
+Version:     0.0.0
+Author:      Lehman Black
+Author URI:  http://www.lehmanblack.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: campaigninator
+*/
