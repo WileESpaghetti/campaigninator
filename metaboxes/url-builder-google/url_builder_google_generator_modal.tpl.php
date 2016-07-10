58db024e241bcbdb99ce266581d30af5db3b6a77
Lehman Black <lehman@developforfun.com>
2016-07-09 23:34:45 -0500

    style fixes

--- metaboxes/url-builder-google/url_builder_google_generator_modal.tpl.php
+++ metaboxes/url-builder-google/url_builder_google_generator_modal.tpl.php
@@ -11,7 +11,7 @@
     >
         <input id="campaigninator_post_id" name="campaigninator_post_id" type="hidden" value="<?php print(get_the_ID()) ?>">
 
-        <p class="alternate">
+        <p class="alternate" style="padding: 1.5em">
             <label>
                 <?php _e('Destination URL', 'campaigninator') ?>
             </label>
@@ -21,7 +21,7 @@
             <a href="<?php the_permalink() ?>"><?php the_permalink() ?></a>
         </p>
         
-        <p>
+        <p style="padding: 1.5em">
             <label>
                 <?php _e('Preset', 'campaigninator'); ?><br>
                 <select>
@@ -33,87 +33,87 @@
                     </option>
                 </select>
             </label>
-            
-            <span class="description"><br>
-                <?php _e('Fill out campaign with preset values', 'campaigninator') ?>
-            </span>
+<!--            -->
+<!--            <span class="description"><br>-->
+<!--                --><?php //_e('Fill out campaign with preset values', 'campaigninator') ?>
+<!--            </span>-->
         </p>
 
-        <p class="alternate">
+        <p class="alternate" style="padding: 1.5em">
             <label>
                 <?php _e('Campaign *', 'campaigninator'); ?><br>
                 <input class="regular-text" id="campaigninator_utm_campaign" name="campaigninator_utm_campaign"
                        placeholder="<?php _e('Why are they coming to you?', 'campaigninator') ?>">
             </label>
-            
-            <br>
-            
-            <span class="description">
-                <?php _e('The individual campaign name, slogan, promo code, etc. for a product.', 'campaigninator') ?>
-            </span>
+<!--            -->
+<!--            <br>-->
+<!--            -->
+<!--            <span class="description">-->
+<!--                --><?php //_e('The individual campaign name, slogan, promo code, etc. for a product.', 'campaigninator') ?>
+<!--            </span>-->
         </p>
 
-        <p>
+        <p style="padding: 1.5em">
             <label>
                 <?php _e('Source *', 'campaigninator'); ?><br>
                 <input class="regular-text" id="campaigninator_utm_source" name="campaigninator_utm_source"
                        placeholder="<?php _e('Where are they coming from?', 'campaigninator') ?>">
             </label>
-            
-            <br>
-            
-            <span class="description">
-                <?php _e('Identify the advertiser, site, publication, etc. that is sending traffic to your property, ' .
-                    'for example: google, newsletter4, billboard.', 'campaigninator'); ?>
-            </span>
+<!--            -->
+<!--            <br>-->
+<!--            -->
+<!--            <span class="description">-->
+<!--                --><?php //_e('Identify the advertiser, site, publication, etc. that is sending traffic to your property, ' .
+//                    'for example: google, newsletter4, billboard.', 'campaigninator'); ?>
+<!--            </span>-->
         </p>
 
-        <p class="alternate">
+        <p class="alternate" style="padding: 1.5em">
             <label>
                 <?php _e('Medium *', 'campaigninator'); ?><br>
                 <input class="regular-text" id="campaigninator_utm_medium" name="campaigninator_utm_medium"
                        placeholder="<?php _e('How are they getting to you?', 'campaigninator') ?>">
             </label>
-            
-            <br>
-            
-            <span class="description">
-                <?php _e('The advertising or marketing medium, example: banner or email newsletter.', 'campaigninator') ?>
-            </span>
+<!--            -->
+<!--            <br>-->
+<!--            -->
+<!--            <span class="description">-->
+<!--                --><?php //_e('The advertising or marketing medium, example: banner or email newsletter.', 'campaigninator') ?>
+<!--            </span>-->
         </p>
 
-        <p>
+        <p style="padding: 1.5em">
             <label>
                 <?php _e('Term', 'campaigninator'); ?><br>
                 <input class="regular-text" id="campaigninator_utm_term"name="campaigninator_utm_term"
                        placeholder="<?php _e('Used to identify PPC keyword', 'campaigninator') ?>">
             </label>
-            
-            <br>
-            
-            <span class="description">
-                <?php _e('Identify paid search keywords. If you\'re manually tagging paid keyword campaigns, you should ' .
-                    'also use utm_term to specify the keyword.', 'campaigninator') ?>
-            </span>
+<!--            -->
+<!--            <br>-->
+<!--            -->
+<!--            <span class="description">-->
+<!--                --><?php //_e('Identify paid search keywords. If you\'re manually tagging paid keyword campaigns, you should ' .
+//                    'also use utm_term to specify the keyword.', 'campaigninator') ?>
+<!--            </span>-->
         </p>
 
-        <p class="alternate">
+        <p class="alternate" style="padding: 1.5em">
             <label>
                 <?php _e('Content', 'campaigninator'); ?><br>
                 <input class="regular-text" id="campaigninator_utm_content" name="campaigninator_utm_content"
                        placeholder="<?php _e('Different ads or place on the page', 'campaigninator') ?>">
             </label>
-            
-            <br>
-            
-            <span class="description">
-                <?php _e('Used to differentiate similar content, or links within the same ad. For example, if you ' .
-                    'have two call-to-action links within the same email message, you can use utm_content and set ' .
-                    'different values for each so you can tell which version is more effective', 'campaigninator') ?>
-            </span>
+<!--            -->
+<!--            <br>-->
+<!--            -->
+<!--            <span class="description">-->
+<!--                --><?php //_e('Used to differentiate similar content, or links within the same ad. For example, if you ' .
+//                    'have two call-to-action links within the same email message, you can use utm_content and set ' .
+//                    'different values for each so you can tell which version is more effective', 'campaigninator') ?>
+<!--            </span>-->
         </p>
 
-        <p>
+        <p style="padding: 1.5em">
             <!-- FIXME not sure if this is best at the top or bottom of the URL builder -->
             <label>
                 <?php _e('Preview', 'campaigninator') ?>
@@ -128,10 +128,10 @@
         </p>
 
         <!-- TODO add waiting animation similar to "update" button -->
-        <span class="spinner"></span>
-        <input name="campaigninator_google_campaign_save_preset" id="campaigninator_google_campaign_save_preset" class="js-campaigninator_google_campaign_save_preset button button-large" type="submit" value="Save as preset">
+<!--        <span class="spinner" style="float: none"></span>-->
+<!--        <input name="campaigninator_google_campaign_save_preset" id="campaigninator_google_campaign_save_preset" class="js-campaigninator_google_campaign_save_preset button button-large" type="submit" value="Save as preset">-->
 
-        <span class="spinner" id="spinner_save_campaign"></span>
+        <span class="spinner" style="float: none" id="spinner_save_campaign"></span>
         <input name="campaigninator_google_campaign_submit" id="campaigninator_google_campaign_submit" class="js-campaigninator_google_campaign_submit button button-primary button-large" type="submit" value="Add campaign">
     </form>
     <div class="notice notice-success inline"><p><?php _e('New campaign created', 'campaigninator') ?></p></div>'
